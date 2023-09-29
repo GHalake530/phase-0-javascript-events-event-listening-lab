76ce4061e9f06e2323ac0d2875eac05f7a36999a
Guyo <guyo.halake@students.moringaschool.com>
2023-09-29 15:38:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+// Select the DOM element
+const buttonElement = document.getElementById('button');
+// Define a callback function
+function clickHandler() {
+  alert('Button was clicked!');
+}
+// Add the event listener
+buttonElement.addEventListener('click', clickHandler);
 }
